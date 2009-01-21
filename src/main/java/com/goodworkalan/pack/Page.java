23160279698605a3e96560be9020d2d02c48e941
Alan Gutierrez <alan@blogometer.com>
2009-01-21 11:34:40 -0600

    - Edited Javadoc for Page class. - Created a script to publish Javadoc.

--- src/main/java/com/goodworkalan/pack/Page.java
+++ src/main/java/com/goodworkalan/pack/Page.java
@@ -2,55 +2,47 @@ package com.goodworkalan.pack;
 
 import java.util.zip.Checksum;
 
-
 /**
- * The <code>Page</code> interface is a specialized application of a
- * {@link RawPage}. It is the strategy participant in the strategy
- * design pattern. The raw page contains only the basic position and byte
- * buffer page attributes. A <code>Page</code> implementation interprets
- * the raw page as a specific page type.
+ * Derived clases implement the address, block, journal formats of an underlying
+ * page in a Pack file. The <code>Page</code> interface is a specialized
+ * application of an underlying page. It is the strategy participant in the
+ * strategy design pattern. Each page has an underlying {@link RawPage}.
+ * <p>
+ * <code>RawPage</code> contains the basic position and byte buffer page
+ * attributes and maintains a set of invalid regions. A <code>Page</code>
+ * implementation interprets the raw page as a specific page type.
  * <h4>Frequent Instanciation and Two-Step Initialization</h4>
  * <p>
- * When we request a page from the the pager, we provide and implementation
- * of <code>Page</code>. If the page exists in the pager, the pager will
- * ensure that the <code>Page</code> is assignable to the class of the
- * specified <code>Page</code>. If the page does not exist in the pager,
- * a new <code>RawPage</code> is created and the specified
- * <code>Page</code> is associated with the <code>RawPage</code>.
+ * When we request a page from the the pager, we give it an implementation of
+ * <code>Page</code> that represents the type request. If the raw page exists in
+ * the pager and it is assocated with the type of page requested, that page is
+ * returned. If it is not in the pager, or if the page associated with the raw
+ * page is a super class of the page requested, the raw page is associated with
+ * the <code>Page</code> implementation given.
  * <p>
- * Thus, we create a lot of <code>Page</code> instances that are not
- * actually initialized. They are used as flags when requesting a page from
- * the <code>Pager</code>. The <code>Pager</code> will use them as
- * actual <code>Page</code> implementations only if page is not available
- * in the <code>Pager</code> or if the associated <code>Page</code>
- * implementation of the page in the <code>Pager</code> is a superclass of
- * the desired <code>Page</code> implementation.
+ * This means that we create a lot of instances of type <code>Page</code> that
+ * are not initialized. They are not initialized because they are not needed.
+ * The pager has the right type of <code>Page</code>. They are simply forgotten
+ * and left for the garbage collector. For this reason, it is important that
+ * implementations of <code>Page</code> have simple default constructors.
  * <p>
- * All <code>Page</code> implementations perform minimal initialization at
- * construction. They are actually initialized by a call to either {@link
- * #create create} or {@link #load load}.
+ * <code>Page</code> instances are not initialized by their default
+ * constructors. They are actually initialized by a call to either
+ * {@link #create create} or {@link #load load}.
  * <h4>Circular RawPage and Page Reference</h4>
  * <p>
- * The circular reference between <code>Page</code> and
- * <code>RawPage</code> is necessary. <code>RawPage</code> is used as
- * the mutex for changes to the underlying byte buffer as well as to the
- * data members of the <code>Page</code> itself. All the <code>Page</code>
- * implementations use the associated <code>RawPage</code> as the lock
- * object in synchronized blocks as opposed to using synchronized methods on
- * the <code>Page</code> itself.
+ * The circular reference between <code>Page</code> and <code>RawPage</code> is
+ * necessary. <code>RawPage</code> is used as the mutex for changes to the
+ * underlying byte buffer as well as to the data members of the
+ * <code>Page</code> itself. All the <code>Page</code> implementations use the
+ * associated <code>RawPage</code> as the lock object in synchronized blocks as
+ * opposed to using synchronized methods on the <code>Page</code> itself.
  * <p>
- * The pager keeps a map by position of the <code>RawPage</code> object an
- * not the <code>Page</code> objects, because the <code>Page</code>
- * associated with a <code>RawPage</code> can change. Specifically, the
- * <code>Page</code> associated with a <code>RawPage</code> can be
- * promoted from a <code>RelocatablePage</code> to a subclass of
- * <code>RelocatablePage</code>
- * <p>
- * The circular reference is necessary. The <code>Page</code>
- * implementation will need read and write the <code>RawPage</code>.The
- * <code>RawPage</code> is the constant that is mapped to the position in
- * the <code>Pager</code>. The <code>RawPage</code> is the lock on the
- * region in the file and it's associated <code>Page</code> can change.
+ * The pager keeps a map by position of the <code>RawPage</code> object an not
+ * the <code>Page</code> objects, because the <code>Page</code> associated with
+ * a <code>RawPage</code> can change. Specifically, the <code>Page</code>
+ * associated with a <code>RawPage</code> can be promoted from a
+ * <code>RelocatablePage</code> to a subclass of <code>RelocatablePage</code>
  * 
  * @see Pager#getPage
  */
@@ -65,9 +57,9 @@ interface Page
 
     /**
      * Initialize the raw page to the specific interpretation implemented by
-     * this page. This method is called from within the <code>Pager</code>
-     * when a new raw page is allocated. The specified raw page will
-     * subsequently be returned by {@link #getRawPage getRawPage}.
+     * this page. This method is called from within the <code>Pager</code> when
+     * a new raw page is allocated. The specified raw page will subsequently be
+     * returned by {@link #getRawPage getRawPage}.
      * 
      * @param rawPage
      *            The raw page.
@@ -78,11 +70,11 @@ interface Page
     public void create(RawPage rawPage, DirtyPageSet dirtyPages);
 
     /**
-     * Load this specific interpretation from the specified the raw page.
-     * This method is called from within the {@link Pager#getPage
-     * getPage} method of the <code>Pager</code> when a page is loaded
-     * from the file. The specified <code>RawPage</code> will be
-     * subsequently returned by {@link #getRawPage getRawPage}.
+     * Load this specific interpretation from the specified the raw page. This
+     * method is called from within the {@link Pager#getPage getPage} method of
+     * the <code>Pager</code> when a page is loaded from the file. The specified
+     * <code>RawPage</code> will be subsequently returned by {@link #getRawPage
+     * getRawPage}.
      * 
      * @param rawPage
      *            The raw page.
@@ -90,7 +82,9 @@ interface Page
      */
     public void load(RawPage rawPage);
     
+    /** FIXME Remove. */
     public void checksum(Checksum checksum);
     
+    /** FIXME Remove. */
     public boolean verifyChecksum(RawPage rawPage, Recovery recovery);
 }
