4198a017d20c35c6bfb7a16bca951b084107a1bf
Alan Gutierrez <alan@blogometer.com>
2009-01-22 00:07:56 -0600

    - Javadoc for NullMoveRecorder.

--- src/main/java/com/goodworkalan/pack/NullMoveRecorder.java
+++ src/main/java/com/goodworkalan/pack/NullMoveRecorder.java
@@ -1,8 +1,22 @@
 package com.goodworkalan.pack;
 
-public final class NullMoveRecorder
+/**
+ * The move recorder used the the move latch list kept by the pager, since the
+ * pager does not actually track moves.
+ * 
+ * @author Alan Gutierrez
+ */
+final class NullMoveRecorder
 implements MoveRecorder
 {
+    /**
+     * Always returns false since this move recorder tracks no pages.
+     * 
+      * @param position
+     *            The position.
+     * 
+     * @return False since the move recorder tracks no pages.
+     */
     public boolean involves(long position)
     {
         return false;
