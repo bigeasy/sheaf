4d9cb0845534c942ac8e2a0de29acc27f858ae29
Alan Gutierrez <alan@blogometer.com>
2010-05-05 03:15:21 -0500

    Updated Mix build for new project language.

--- src/mix/java/com/goodworkalan/sheaf/SheafProject.java
+++ src/mix/java/com/goodworkalan/sheaf/SheafProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.sheaf.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,16 +9,16 @@ public class SheafProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/sheaf/0.1"))
+                .produces("com.github.bigeasy.sheaf/sheaf/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.goodworkalan/region/0.1"))
+                        .include("com.goodworkalan/region/0.+1")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
-                        .artifact(new Artifact("org.testng/testng/5.10"))
+                        .include("org.mockito/mockito-core/1.6")
+                        .include("org.testng/testng-jdk15/5.10")
                         .end()
                     .end()
                 .end()
