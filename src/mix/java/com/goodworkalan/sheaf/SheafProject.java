1b2742dfbebea184e7307b9e879d4b205cb64265
Alan Gutierrez <alan@blogometer.com>
2010-06-03 01:41:20 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/sheaf/SheafProject.java
+++ src/mix/java/com/goodworkalan/sheaf/SheafProject.java
@@ -4,22 +4,26 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class SheafProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Sheaf.
+ *
+ * @author Alan Gutierrez
+ */
+public class SheafProject implements ProjectModule {
+    /**
+     * Build the project definition for Sheaf.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.sheaf/sheaf/0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.region/region/0.+1")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.mockito/mockito-core/1.6")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.region/region/0.+1")
+                    .development("org.mockito/mockito-core/1.6")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
